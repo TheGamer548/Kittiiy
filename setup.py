b408656989ed5a59c7ae244ee0f3e425a2e4ea26
TheGamer548 <106841300+TheGamer548@users.noreply.github.com>
2022-06-16 16:39:52 +0100

    setup.py
    The Setup of Kittiy

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='Kittiy',
+    version='0.1.0',    
+    description='Kittiy',
+    url='https://github.com/TheGamer548/Kittiiy',
+    author='Coder10',
+    author_email='shudson@anl.gov',
+    license='BSD 2-clause',
+    packages=['kittiy'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
